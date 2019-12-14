00389a36fabb6d9f994d71c74d73dfada068c69c
jaclynchorton <jaclynchorton@gmail.com>
2019-12-14 13:51:42 -0800

    Commit iteratorsWithItertools-InfiniteProcesses.py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Iterators with itertools
- 
+ # The Itertools module allows us to iterate and go through our data in different ways.
---
+++ iteratorsWithItertools-InfiniteProcesses.py
@@ -0,0 +1,12 @@
+#--------------------From Learning the Python Standard Library----------------
+
+#---------------------------------Itertools-----------------------------------
+import itertools
+
+# This module will create an infinite counting mechanism
+
+#--------------------------------Infinite Counting----------------------------
+for x in itertools.count(50):
+    print(x)
+    if x == 80:
+        break;
