8ee60c5588507343469fead21de043c1538a4c19
jaclynchorton <jaclynchorton@gmail.com>
2019-12-14 14:23:11 -0800

    Commit for Infinite Cycling

--- iteratorsWithItertools-InfiniteProcesses.py
+++ iteratorsWithItertools-InfiniteProcesses.py
@@ -10,3 +10,8 @@ for x in itertools.count(50, 5):
     print(x)
     if x == 80:
         break;
+    
+#-------------------------------Infinite Cycling------------------------------
+# Infinitely cycling through an input
+for c in itertools.cycle("RACECAR"):
+    print(c)
