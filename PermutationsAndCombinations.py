ba8de430e54e3fd488fa06fd0c5c7a941511b37e
jaclynchorton <jaclynchorton@gmail.com>
2019-12-14 14:50:53 -0800

    Initial commit of PermutationsAndCombinations.py

---
+++ PermutationsAndCombinations.py
@@ -0,0 +1,8 @@
+#-----------------------From Learning the Python Standard Library--------------
+
+#-------------------------------Review of Permutations-------------------------
+# A way, especially of one of several possible variations, in which a set or number of
+# things can be ordered or arranged
+# Permutations of same items and different order
+
+
